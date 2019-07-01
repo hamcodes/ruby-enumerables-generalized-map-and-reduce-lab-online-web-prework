238c21f662c06127ef3c2fbbec95e4bee0dbf007
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-07-01 19:03:28 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,3 +2,8 @@ describe 'my own map' do
   it "returns an array with all values made negative" do
     expect(map([1, 2, 3, -9]){|n| n * -1}).to eq([-1, -2, -3, 9])
   end
+
+  it "returns an array with the original values" do
+    dune = ["paul", "gurney", "vladimir", "jessica", "chani"]
+    expect(map(dune){|n| n}).to eq(dune)
+  end
