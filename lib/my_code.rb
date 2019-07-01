b965c5698b89bd5706be8bda0ce18f20845ade6d
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-07-01 19:10:24 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,6 +1,6 @@
 def my_own_map(element1,element2)
     yield(#{element1}#{element2})
   end
-    expect(map([1, 2, 3, -9]){|n| n * -1}).to eq([-1, -2, -3, 9])
+    my_own_map(map([1, 2, 3, -9]){|n| n * -1}).to eq([-1, -2, -3, 9])
   end
 
