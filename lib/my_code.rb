3d294e0f5084ca46ec3bbd5aae0a4d7910fd7e5f
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-07-01 19:02:41 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,9 +1,4 @@
-def map(s)
-  new = []
-  i = 0
-  while i < s.length
-    new.push(yield(s[i]))
-    i += 1
+describe 'my own map' do
+  it "returns an array with all values made negative" do
+    expect(map([1, 2, 3, -9]){|n| n * -1}).to eq([-1, -2, -3, 9])
   end
-  new
-end
