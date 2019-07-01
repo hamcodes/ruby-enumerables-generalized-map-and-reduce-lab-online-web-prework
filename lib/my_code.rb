3edada7281678ff223d0c9690e9c83d56f63f6d4
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-07-01 19:05:44 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,7 +2,3 @@ def my_own_map
     expect(map([1, 2, 3, -9]){|n| n * -1}).to eq([-1, -2, -3, 9])
   end
 
-  it "returns an array with the original values" do
-    dune = ["paul", "gurney", "vladimir", "jessica", "chani"]
-    expect(map(dune){|n| n}).to eq(dune)
-  end
