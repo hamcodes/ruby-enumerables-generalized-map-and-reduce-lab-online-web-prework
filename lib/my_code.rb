41ffdf58cc0c101b08714d491db3249c42a7ac35
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-07-01 18:55:35 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,4 +1,4 @@
 def my_own_map(element1,element2)
-  yield("A #{element1} and #{element2})
-  my_own_map([1, 2, 3, -9]){|n| n * n}
+  yield(#{element1} and #{element2})
 end
+  my_own_map([1, 2, 3, -9]){|n| n * n}
