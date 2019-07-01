5167433fe12b198d6ebc4a8e37a5cadfe272a0e1
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-07-01 19:21:26 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,4 +1,4 @@
-def my_own_map
+def map(s)
   new = []
   i = 0
   while i < s.length
@@ -6,4 +6,19 @@ def my_own_map
     i += 1
   end
   new
+end
+
+def reduce(s, sp=nil)
+  if sp
+    accum = sp
+    i = 0
+  else
+    accum = s[0]
+    i = 1
+  end
+  while i < s.length
+    accum = yield(accum, s[i])
+    i += 1
+  end
+  accum
 end
